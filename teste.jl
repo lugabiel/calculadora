f6178788a1f574078e5e30f6b832abbb84719398
lugabiel <36977837+lugabiel@users.noreply.github.com>
2020-10-06 02:37:59 -0300

    code readability
    v1.0 - organizando comentarios.

--- teste.jl
+++ teste.jl
@@ -1,17 +1,16 @@
-#go to directory
+#--define directory of .l and .html files
 cd(raw"C:\julia-playgroung\calculadora")
-#include(teste.jl)
-#-- Teste de Aplicação web 
+
+#--teste de Aplicação web 
 using Blink;println("using Blink")
-  
 Blink.AtomShell.install();
 #varinfo(Blink)#whos(Blink)
+
+#--publish window with a massage
 w = Window();
-#publish window with a massage
-#body!(w,"Hello Julia With Blink")
 f = open("test.html") do file
-              read(file,String)
-      end 
-      
+            read(file,String)
+    end 
+
 #publish window w with html file f
 body!(w,f)
