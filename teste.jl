e29753e0142cfbfda0cccafcdc5c119ca9dbcb63
lugabiel <36977837+lugabiel@users.noreply.github.com>
2020-10-05 23:42:53 -0300

    Initial commit

---
+++ teste.jl
@@ -0,0 +1,14 @@
+#-- Teste de Aplicação web 
+using Blink;println("using Blink")
+  
+Blink.AtomShell.install()
+#varinfo(Blink)#whos(Blink)
+w = Window()
+#publish window with a massage
+#body!(w,"Hello Julia With Blink")
+f = open("test.html") do file
+              read(file,String)
+      end 
+      
+#publish window w with html file f
+body!(w,f)
